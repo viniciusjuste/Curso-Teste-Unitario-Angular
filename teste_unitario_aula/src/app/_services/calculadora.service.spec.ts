73d5e914eabda2e0b3b905429800dd912407d8f6
Vinícius Juste <157420992+viniciusjuste@users.noreply.github.com>
2025-06-26 20:58:22 -0300

    feat: usando um spy

--- teste_unitario_aula/src/app/_services/calculadora.service.spec.ts
+++ teste_unitario_aula/src/app/_services/calculadora.service.spec.ts
@@ -1,13 +1,18 @@
 import { TestBed } from '@angular/core/testing';
 
 import { CalculadoraService } from './calculadora.service';
+import { LoggerService } from './logger.service';
 
 describe('CalculadoraService', () => {
   let service: CalculadoraService;
+  let loggerSpy: any
 
   beforeEach(() => {
+    loggerSpy = jasmine.createSpyObj('LoggerService', ['log']);
     TestBed.configureTestingModule({
-      providers: [CalculadoraService],
+      providers: [CalculadoraService,
+        { provide: LoggerService, useValue: loggerSpy }
+      ],
     });
     service = TestBed.inject(CalculadoraService);
   });
@@ -20,5 +25,10 @@ describe('CalculadoraService', () => {
     expect(service.calcular(2, 3, 'multiplicacao')).toBe(6);
     expect(service.calcular(6, 3, 'divisao')).toBe(2);
     expect(service.calcular(6, 0, 'divisao')).toBeNaN();
+  });
+
+  it('Operação não existe', () => {
+    expect(service.calcular(2, 3, 'modulo')).toBeNull();
+    expect(loggerSpy.log).toHaveBeenCalledTimes(1);
   });
 });
--- teste_unitario_aula/src/app/_services/calculadora.service.ts
+++ teste_unitario_aula/src/app/_services/calculadora.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core';
+import { LoggerService } from './logger.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CalculadoraService {
 
-  constructor() { }
+  constructor(private logger: LoggerService) { }
 
-  calcular(num1 : number, num2: number, operacao: string): number {
+  calcular(num1: number, num2: number, operacao: string): number | null {
     switch (operacao) {
       case 'soma':
         return num1 + num2;
@@ -16,10 +17,10 @@ export class CalculadoraService {
       case 'multiplicacao':
         return num1 * num2;
       case 'divisao':
-        return num2 !== 0 ? num1 / num2 : NaN; 
+        return num2 !== 0 ? num1 / num2 : NaN;
       default:
-        throw new Error('Operação inválida');
+        this.logger.log(`Operação inválida: ${operacao}`);
+        return null;
     }
-
   }
 }
