d95fb091c17d8c5d1210928889fe73294f986e0c
Vinícius Juste <157420992+viniciusjuste@users.noreply.github.com>
2025-06-26 20:15:21 -0300

    feat: cria uma função de calcular dois numeros baseado em uma entrada

--- teste_unitario_aula/src/app/_services/calculadora.service.ts
+++ teste_unitario_aula/src/app/_services/calculadora.service.ts
@@ -6,4 +6,20 @@ import { Injectable } from '@angular/core';
 export class CalculadoraService {
 
   constructor() { }
+
+  calcular(num1 : number, num2: number, operacao: string): number {
+    switch (operacao) {
+      case 'soma':
+        return num1 + num2;
+      case 'subtracao':
+        return num1 - num2;
+      case 'multiplicacao':
+        return num1 * num2;
+      case 'divisao':
+        return num2 !== 0 ? num1 / num2 : NaN; 
+      default:
+        throw new Error('Operação inválida');
+    }
+
+  }
 }
