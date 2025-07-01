a377686a9222ad5c9e4ba3303fc1e989d95cff29
Vinícius Juste <157420992+viniciusjuste@users.noreply.github.com>
2025-07-01 19:39:39 -0300

    feat: implementa as funções do service

--- teste_unitario_aula/src/app/app.component.ts
+++ teste_unitario_aula/src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TodosService } from './_services/todos.service';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,28 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'teste_unitario_aula';
+
+  constructor(private todoService : TodosService) {}
+
+  ngOnInit(){
+    this.todoService.getAll().subscribe({
+      next: (todos) => {
+        console.log(todos);
+      },
+      error: (error) => {
+        console.error('Error fetching todos:', error);
+      }
+    });
+
+    this.todoService.getById(1).subscribe({
+      next: (todo) => {
+        console.log(todo);
+      },
+      error: (error) => {
+        console.error('Error fetching todo by ID:', error);
+      }
+    });
+  }
 }
